821e6c242400c90cc474baa0984eee72f1ca2f4a
borovoykirill <borovoy.kirill93@gmail.com>
2020-03-02 18:08:18 +0300

    Homework2: Kirill Baravoy

--- task2.py
+++ task2.py
@@ -10,5 +10,5 @@ for i in range(len(values)):
     result.update({keys[i]: values[i]})
 if len(keys) > len(values):
     for i in range(l2):
-        result.update({keys[len(values)+i]: "None"})
+        result.update({keys[len(values) + i]: "None"})
 print(result)
